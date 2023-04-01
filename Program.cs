2282df6d635081e7d3e356d17b6c6170b1fd101e
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-04-01 15:27:15 -0300

    implementado ordenação por inserção + busca linear otimizada

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] a = {5,2,3,4,8,8,9,12};
+InsertSort(a);
+System.Console.WriteLine(LinearSearch(a, 12));
+
+void InsertSort(int[] array)
+{
+    int index_clone, value_clone;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        index_clone = i;
+        value_clone = array[i];
+
+        while (index_clone > 0 && array[index_clone - 1] > value_clone)
+        {
+            array[index_clone] = array[index_clone - 1];
+            index_clone--; 
+        }
+        array[index_clone] = value_clone;
+    }
+}
+
+int LinearSearch(int[] array, int k)
+{
+    for (int i = 0; i < array.Length && array[i] <= k; i++)
+    {
+        if(k == array[i])
+            return i;
+    }
+    return -1;
+}
